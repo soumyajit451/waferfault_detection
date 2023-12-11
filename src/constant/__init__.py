dabfeba3aa76a822735fcefbfffa3f99f45007e4
soumyajit <soumyajitsahu2013rta@gmail.com>
2023-12-11 19:35:53 +0530

    file changes

--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -2,11 +2,11 @@ import os
 
 
 AWS_S3_BUCKET_NAME = "wafer-fault"
-MONGO_DATABASE_NAME = "pwskills"
+MONGO_DATABASE_NAME = "silu"
 MONGO_COLLECTION_NAME = "waferfault"
 
 TARGET_COLUMN = "quality"
-MONGO_DB_URL="mongodb+srv://snshrivas:Snshrivas@cluster0.u46c4.mongodb.net/?retryWrites=true&w=majority"
+MONGO_DB_URL="mongodb+srv://soumyajit:soumyajit@cluster0.etgn6yo.mongodb.net/?retryWrites=true&w=majority"
 
 MODEL_FILE_NAME = "model"
 MODEL_FILE_EXTENSION = ".pkl"
