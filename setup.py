cd6f2680842fb67be7bbca02d0930b83d54548e7
soumyajit <soumyajitsahu2013rta@gmail.com>
2023-12-11 18:26:11 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Soumyajit",
+    author_email="soumyajitsahu2013rta@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
